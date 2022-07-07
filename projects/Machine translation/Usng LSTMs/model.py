83c805fb3a5b1f079b333b1c5ab0211f1f34d197
Swayam <74960567+practice404@users.noreply.github.com>
2022-07-07 12:49:03 +0530

    added .predict() model.py

--- projects/Machine translation/Usng LSTMs/model.py
+++ projects/Machine translation/Usng LSTMs/model.py
@@ -36,6 +36,7 @@ class Decoder(nn.Module):
         predictions = predictions.squeeze(0)
         return predictions, hidden, cell
 
+
 class Seq2Seq(nn.Module):
     def __init__(self, encoder,  decoder, eng_vocab_size, device):
         super(Seq2Seq, self).__init__()
@@ -47,14 +48,32 @@ class Seq2Seq(nn.Module):
     def forward(self, source, target, teacher_force_ratio=0.5):
         batch_size = source.shape[1]
         target_len = target.shape[0]
-        outputs = torch.zeros((target_len, batch_size, self.eng_vocab_size)).to(self.device)
+        outputs = torch.zeros(
+            (target_len, batch_size, self.eng_vocab_size)).to(self.device)
         hidden, cell = self.encoder(source)
         x = target[0]
+        print(x)
         for t in range(1, target_len):
             output, hidden, cell = self.decoder(x, hidden, cell)
             outputs[t] = output
             best_guess = output.argmax(1)
-            x = target[t] if random.random() < teacher_force_ratio else best_guess
+            x = target[t] if random.random(
+            ) < teacher_force_ratio else best_guess
         return outputs
 
+    def predict(self, source, max_len=100):
+        result = []
+        result.append(1)  # index of <SOS> token
+        hidden, cell = self.encoder(source)
+        hidden, cell = hidden.unsqueeze(1), cell.unsqueeze(1)
+        x = torch.tensor([1]).to(self.device)
+        for t in range(1, max_len):
+            output, hidden, cell = self.decoder(x, hidden, cell)
+            best_guess = output.argmax()
+            result.append(best_guess)
+            print(best_guess.data.item())
+            if best_guess.data.item() == 2:
+                return torch.tensor(result).to(self.device)
+        result.append(2)  # index of <EOS> token
 
+        return torch.tensor(result).to(self.device)
